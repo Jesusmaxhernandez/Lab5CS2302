5be90eb5b38021fe562c69e8474a5b1bb3f383ba
Jesusmaxhernandez <38057904+Jesusmaxhernandez@users.noreply.github.com>
2018-11-27 23:57:46 -0700

    Create Heap.py

---
+++ Heap.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 26 14:31:37 2018
+
+@author: JesusMHernandez
+"""
+
+class Heap:
+    
+    def __init__(self):
+        self.heap_array = []
+    def insert(self, k):
+        """
+        Method to insert into the Heap 
+        """
+        self.heap_array.append(k)
+        self.up_heap()
+        
+    def up_heap(self):
+        lastItem = len(self.heap_array) - 1
+              
+        while(self.heap_array[lastItem] < self.heap_array[(lastItem - 1)//2]):
+            parent = ((lastItem - 1)//2)
+            
+            temp = self.heap_array[lastItem]
+            self.heap_array[lastItem] = self.heap_array[(lastItem - 1)//2]
+            self.heap_array[(lastItem - 1)//2] = self.heap_array[lastItem]
+            
+    def 
+    #TODO: Complete implentation
+    def extract_min(self):
+        if self.is_empty():
+            return None
+        min_elem = self.heap_array.append[0]
+        return min_elem
+    
+    def is_empty(self):
+        return len(self.heap_array) == 0
+    
+def heap_sort(lst):
+    heap = Heap()
+    result = [] 
+    for elem in lst:
+        heap.insert(elem)
+    while not heap.is_empty():
+        result.append(heap.extract_min())
+    return result
