31c26a1fa22f44d0715149a6430a028b122a96b4
Jesusmaxhernandez <38057904+Jesusmaxhernandez@users.noreply.github.com>
2018-11-29 20:53:18 -0700

    Update HeapSort.py

--- HeapSort.py
+++ HeapSort.py
@@ -17,26 +17,29 @@ class HeapSort():
     
 
     def makeHeap(self, lst):
+        """
+        makes a valid heap out of given numbers
+        """
         for i in range(len(lst)):
             h.insert(lst[i])
             
         h.print_lst()
         
-    #based on zybooks implementations
-    def down_heap(self, node_index, heap_list, list_size):
+
+    def down_heap(self, node_index, heap_lst, lst_size):
         """
         Gets items in the front and swaps them until valid Heap
         """
         child_index = 2 * node_index + 1
-        value = heap_list[node_index]
-        while child_index < list_size:
+        value = heap_lst[node_index]
+        while child_index < lst_size:
             # Find the max among the node and all the node's children
             max_value = value
             max_index = -1
             i = 0
-            while i < 2 and i + child_index < list_size:
-                if heap_list[i + child_index] > max_value:
-                    max_value = heap_list[i + child_index]
+            while i < 2 and i + child_index < lst_size:
+                if heap_lst[i + child_index] > max_value:
+                    max_value = heap_lst[i + child_index]
                     max_index = i + child_index
                 i = i + 1
                                         
@@ -44,18 +47,21 @@ class HeapSort():
                 return
     
             # Swap heap_list[node_index] and heap_list[max_index]
-            temp = heap_list[node_index]
-            heap_list[node_index] = heap_list[max_index]
-            heap_list[max_index] = temp
+            temp = heap_lst[node_index]
+            heap_lst[node_index] = heap_lst[max_index]
+            heap_lst[max_index] = temp
             
             node_index = max_index
             child_index = 2 * node_index + 1
     
     
-    #based on zybooks implementations
+
     def heap_sort(self, lst):
-        
-        # Heapify numbers list
+        """
+        Swaps the first and last elements on heap array to sort then uses
+        down heap to keep vaid heap 
+        """
+        # Heapify 
         i = len(lst) // 2 - 1
         while i >= 0:
             self.down_heap(i, lst, len(lst))
@@ -63,7 +69,7 @@ class HeapSort():
                     
         i = len(lst) - 1
         while i > 0:
-            # Swap numbers[0] and numbers[i]
+            # Swap function
             temp = lst[0]
             lst[0] = lst[i]
             lst[i] = temp
